93afd10c32e293808063175921bac0055f0533b2
Ricci Adams <git@ricciadams.com>
2021-02-25 15:25:30 -0800

    Remove receipt validation

--- Source/main.m
+++ Source/main.m
@@ -8,24 +8,7 @@
 
 #import <Cocoa/Cocoa.h>
 
-#if USE_RECEIPT_VALIDATION
-#include "ReceiptValidation.m"
-#endif
-
 int main(int argc, char *argv[])
 {
-#if USE_RECEIPT_VALIDATION && !DEBUG
-    __block int returnCode = 0;
-
-    ReceiptValidationCheck(^{
-        returnCode = NSApplicationMain(argc,  (const char **) argv);
-    }, ^{
-        returnCode = 173;
-        exit(173);
-    });
-    
-    return returnCode;
-#else
     return NSApplicationMain(argc,  (const char **) argv);
-#endif
 }
